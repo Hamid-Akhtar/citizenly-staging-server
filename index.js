fa0a8f5c3f05593d65a9cdae2ac26d69af2e8ee0
Hamid Akhtar <hamidakhtar.works@gmail.com>
2021-10-14 15:37:14 -0400

    new improvements

--- index.js
+++ index.js
@@ -1,6 +1,21 @@
 // Copy the .env.example in the root into a new .env file with your API keys
 const { resolve } = require('path');
 const { Sequelize, DataTypes, Op } = require('sequelize');
+const multer  = require('multer');
+const http = require('http');
+const url = require('url') ;
+const path = require("path");
+
+const storage = multer.diskStorage({
+  destination: function (req, file, cb) {
+    cb(null, 'images/')
+  },
+  filename: function (req, file, cb) {
+    cb(null, Date.now() + path.extname(file.originalname))
+  }
+})
+
+const upload = multer({ dest: 'images/', storage });
 const env = require('dotenv').config({ path: resolve(__dirname, '../../.env') });
 const cors = require('cors');
 const passport = require('passport')
@@ -37,35 +52,28 @@ const User = sequelize.define('representatives', {
   }
 }, { updatedAt: false, createdAt: false, initialAutoIncrement: false });
 
-const RepresentativeApplication = sequelize.define('representative_applications', {
+const RepresentativeApplication = sequelize.define('representative_requests', {
   // Model attributes are defined here
-  name: {
-    type: DataTypes.STRING,
-    allowNull: false
-  },
-  address: {
-    type: DataTypes.STRING,
-    allowNull: false
-  },
-  phones: {
-    type: DataTypes.STRING,
-    allowNull: false
-  },
-  urls: {
-    type: DataTypes.STRING,
-    allowNull: false
-  },
-  division: {
-    type: DataTypes.STRING,
+  official: {
+    type: DataTypes.JSON,
     allowNull: false
   },
   office: {
+    type: DataTypes.JSON,
+    allowNull: false
+  },
+  divisions: {
+    type: DataTypes.JSON,
+    allowNull: false
+  },
+  divisionId: {
     type: DataTypes.STRING,
     allowNull: false
   },
-  divisionType: {
+  id: {
     type: DataTypes.STRING,
-    allowNull: false
+    allowNull: false,
+    primaryKey: true
   }
 }, { updatedAt: false, createdAt: false, initialAutoIncrement: false });
 
@@ -161,10 +169,11 @@ const respondToClient = (error, responseData, res) => {
  */
 app.post('/add-new-rep', async (req, res) => {
   try {
-    const rep = RepresentativeApplication.create({...req.body, id : uuid()});
+    await RepresentativeApplication.create({...req.body, id : uuid()});
     res.status(200).json({message: "Successful Submitted Your Application"});
   }
   catch (err) {
+    console.log(err);
     res.status(400).json({ error: "Something went wrong, unable to fetch results!" });
   }
 });
@@ -227,6 +236,12 @@ app.get('/get-verification-session/:verificationSessionId', async (req, res) => 
   });
 });
 
+app.post("/upload_image", upload.single('photo'), async (req, res)=>{
+  var hostname = req.headers.host; 
+  const uri = `http://${hostname}/${req.file.filename}`;
+  res.status(200).json({photo_url: uri});
+});
+
 /**
  * Call Civics API
  */
@@ -246,15 +261,53 @@ app.get('/representatives', async (req, res) => {
       }
     });
     if (resp) {
-      console.log("local DB");
-      const divisionsData = resp.response;
+      let divisionsData = resp.response;
+      const keysOfDiv = Object.keys(divisionsData.divisions);
+      const rep = await RepresentativeApplication.findAll({
+        where: {
+          divisionId: {
+            [Op.in]: keysOfDiv
+          }
+        }   
+      });
+
+      rep.map(r=>{
+        if(r) {
+          const repre = r.toJSON();
+          let officialIndex = divisionsData.officials.push(repre.official) - 1;
+          let officeIndex = divisionsData.offices.push(repre.office) - 1;
+          if(divisionsData.divisions[repre.divisionId] && divisionsData.divisions[repre.divisionId].officeIndices){
+            divisionsData.divisions[repre.divisionId].officeIndices.push(officeIndex);
+            divisionsData.offices[officeIndex].officialIndices = [officialIndex];
+          }
+        }
+      });
+      
       res.json({ ...divisionsData })
     }
     else {
-      console.log("Calling API");
       const responseFromCivic = await axios.get(url);
-      const data = responseFromCivic.data;
+      let data = responseFromCivic.data;
+      const keysOfDiv = Object.keys(data.divisions);
       await Representative.create({ searchTerm: searchTerm.toLowerCase(), id: searchTerm.toLowerCase(), response: data });
+      const rep = await RepresentativeApplication.findAll({
+        where: {
+          divisionId: {
+            [Op.in]: keysOfDiv
+          }
+        }   
+      });
+      rep.map(r=>{
+        if(r) {
+          const repre = r.toJSON();
+          let officialIndex = data.officials.push(repre.official) - 1;
+          let officeIndex = data.offices.push(repre.office) - 1;
+          if(data.divisions[repre.divisionId] && data.divisions[repre.divisionId].officeIndices){
+            data.divisions[repre.divisionId].officeIndices.push(officeIndex);
+            data.offices[officeIndex].officialIndices = [officialIndex];
+          }
+        }
+      });
       res.json(data);
     }
   }
@@ -268,16 +321,18 @@ app.get('/representatives', async (req, res) => {
 /*
  * Handle static assets and other pages
  */
-app.use(express.static('./client'));
+app.use(express.static('./images'));
+
+
 
 /*
  * Handle 404 responses
- */
+
 app.use(function (req, res, next) {
   const path = resolve(__dirname, '../client/404.html');
   res.status(404).sendFile(path);
 })
-
+ */
 
 // Start server
 const localPort = process.env.PORT || 8080;
