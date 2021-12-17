f947e95d4f8a0806c4296edf777ff26387755df7
= <hamidakhtar.works@gmail.com>
2021-12-17 18:41:15 -0500

    Allow new host

--- index.js
+++ index.js
@@ -12,7 +12,9 @@ const app = express();
 const allowlist = ['http://citizenreps.s3-website-us-east-1.amazonaws.com', 
                    'http://www.citizenly.com',
                    'http://citizenlyadmin.s3-website-us-east-1.amazonaws.com', 
-                   'http://citizenlyui.s3-website-us-east-1.amazonaws.com'];
+                   'http://citizenlyui.s3-website-us-east-1.amazonaws.com',
+                    'https://dev.citizenopolis.com'
+                  ];
 const corsOptionsDelegate = (req, callback) => {
   let corsOptions;
 
