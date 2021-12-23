1cab65f39f2f19a8f967ae41932f3cbdbe4d0d18
= <=>
2021-12-24 02:01:29 +0500

    Credentials Changed

--- passport.js
+++ passport.js
@@ -6,7 +6,7 @@ module.exports = (app, passport) => {
     passport.use(
         'login', new LocalStrategy(
         (username, password, done) => {
-                if(username === "admin" && password === "admin"){
+                if(username === "chris.knotz@citizenly.com" && password === "clTi.1616#"){
                     return done(null, username);
                 } else {
                     return done("Unauthorized Access", false);
